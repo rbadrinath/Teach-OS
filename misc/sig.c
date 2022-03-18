1c96fc9a7f13d27052ae18a39bdbaf937fe51f35
Badrinath R <r.badrinath@gmail.com>
2022-03-18 19:31:24 +0530

    signal handling

---
+++ misc/sig.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
+
+int g=0;
+
+void handler(int signo)
+{
+  if (signo ==  SIGUSR1)
+    printf("received SIGUSR1 when g=%d\n",g);
+  g=100;
+}
+
+int main(void)
+{
+  if (signal(SIGUSR1, handler) == SIG_ERR) {
+       perror("setting signal failed");
+       exit(1);
+  }
+  sleep(15);
+  printf("Finished g=%d\n",g);
+}
