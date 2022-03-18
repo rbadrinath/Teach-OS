66e7f725c5fa0a51b9a73a0673729f8427c81464
Badrinath R <r.badrinath@gmail.com>
2022-03-18 18:19:29 +0530

    Investigate address space

---
+++ misc/address.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int g=0;
+void main() {
+	int x;
+	int * a;
+	int z[100];
+ 	a = (int *)malloc(10);
+   	printf("writecode) is at %p\n",write);
+   	printf("main(code) is at %p\n",main);
+   	printf("g   (data) is at %p\n",&g);
+   	printf("a   (heap) is    %p\n",a);
+   	printf("x   (stak) is at %p\n",&x);
+   	printf("a   (stak) is at %p\n",&a);
+   	printf("z   (stak) is at %p\n",&z);
+	printf(" ......... you may want to use pmap <PID> to see the address space\n");
+	sleep(10000);
+}
+
