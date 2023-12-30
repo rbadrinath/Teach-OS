f6c3c5c17913f82f270bb78dd5cb258c0d095064
Badrinath R <r.badrinath@gmail.com>
2023-12-30 20:18:54 +0530

    minor updates

--- procs/syscall.s
+++ procs/syscall.s
@@ -2,6 +2,7 @@
 # taken from: https://cs.lmu.edu/~ray/notes/gasexamples/
 # See the above site for more on x86-64 assembly
 # Note: This is pure assembly and uses no C libraries
+# We dont want gcc to invoke ld in the usual way, so compile differently.
 # To compile:
 #     $ gcc -c syscall.s
 #     $ ld syscall.o
@@ -12,14 +13,14 @@
 
         .text
 _start:
-        # write(1, message, 13)
+        # write(1, message, 13)		# see man page WRITE(2)
         mov     $1, %rax                # system call 1 is write
         mov     $1, %rdi                # file handle 1 is stdout
         mov     $message, %rsi          # address of string to output
         mov     $len, %rdx               # number of bytes
         syscall                         # invoke operating system to do the write
 
-        # exit(0)
+        # exit(0)			# see man page EXIT(2)
         mov     $60, %rax               # system call 60 is exit
         xor     %rdi, %rdi              # we want return code 0
         syscall                         # invoke operating system to exit
