79cc8a65ee65424a2c15a7a84dbe9abe23814efd
Badrinath R <r.badrinath@gmail.com>
2023-12-31 14:07:08 +0530

    kill-example

--- procs/fork_and_kill.c
+++ procs/fork_and_kill.c
@@ -9,14 +9,14 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-/* Simple program that creates a copy process by using fork*/
+#include <signal.h>
+
 void main(){
 	int child_pid;
 	int status;
 	printf("1 PID = %d\n",(int)getpid());   // See this once only
 	child_pid = fork();   // This is where all the aciton is
 	printf("2 PID = %d, %d\n",(int)getpid(),child_pid); // See this twice
-// Uncomment this for both processes to loop
 	if (child_pid == 0 ) {
 		printf("Child looping\n");
 		while (1) {} // short form for an infinite loop
@@ -24,8 +24,12 @@ void main(){
 		// printf("Parent looping\n");
 		// while (1) {}
 		// alternately
-		printf("Parent waiting for child to finish\n");
+		printf("Process ready to %d send a kill to Process %d \n",
+				getpid(), child_pid);
+		getchar();
+		kill(child_pid,SIGTERM);
+		printf("Check that process %d exited\n", child_pid);
+		getchar();
 		wait(&status);
-		printf("Child termineted.... so parent exiting \n");
 	}
 }
