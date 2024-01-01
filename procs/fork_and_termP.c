65cc87061ab75e59d11714eaf9be82d94166eaf4
Badrinath R <r.badrinath@gmail.com>
2024-01-01 16:59:52 +0530

    reorganized

---
+++ procs/fork_and_termP.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <signal.h>
+
+void main(){
+	int child_pid;
+	int status;
+	printf("1 PID = %d\n",(int)getpid());   // See this once only
+	child_pid = fork();   // This is where all the aciton is
+	printf("2 PID = %d, %d\n",(int)getpid(),child_pid); // See this twice
+	if (child_pid == 0 ) {
+		// child aims to terminate the parent process
+		int parent_pid=getppid();
+		printf("Process ready to %d send a kill to Process %d \n",
+				getpid(), parent_pid);
+		getchar();
+		kill(parent_pid,SIGTERM);
+		printf("Check that process %d exited\n", parent_pid);
+		getchar();
+		wait(&status);
+	} else {
+		// parent loops indefinitely
+		 while (1) {}
+	}
+}
