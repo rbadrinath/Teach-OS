bdf0ce7909c8d5ecea62ac596d96cf87a4651ad5
Badrinath R <r.badrinath@gmail.com>
2024-01-28 07:09:19 +0530

    beautify code

--- files/usingdup.c
+++ files/usingdup.c
@@ -1,23 +1,46 @@
 // open the file twice
 #include "myincludes.h"
-void main1(){
+#include <string.h>
+#define CMP_ARGV(I,X) (strcmp(argv[I],X)==0)
+void withoutdup(){
 	char buf[100];
+
+	// open same file twice
 	int fd1 = open("xyz",O_RDONLY);
 	int fd2 = open("xyz",O_RDONLY);
+
+	// read from first fd and print on screen
 	int n = read(fd1,buf,10);
 	write(1,buf,10);
+	
+	// read from second fd and print on screen
 	n = read(fd2,buf,10);
 	write(1,buf,10);
+
+	// they have two different internal structures/information
 }
-void main2(){
+void withdup(){
 	char buf[100];
+
+	// open the file only once
 	int fd1 = open("xyz",O_RDONLY);
+	// duplicate the fd ... (NOT open again)
 	int fd2 = dup(fd1);
+
+	// read using fd1 and print on the screen
 	int n = read(fd1,buf,10);
 	write(1,buf,10);
+
+	// read using fd2 and print on the screen
 	n = read(fd2,buf,10);
 	write(1,buf,10);
+
+	// the two fds share the same internal structures
 }
-void main(){
-	main2();
+void main(int argc, char *argv[]){
+	if (argc > 1 ) {
+		if (CMP_ARGV(1,"--nodup") || CMP_ARGV(1,"-n"))
+			withoutdup();
+	} else
+			withdup();
 }
