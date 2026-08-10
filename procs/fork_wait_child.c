33bf9e32ed40d383f23727319acbc47b35855aed
Badrinath R <r.badrinath@gmail.com>
2026-08-10 06:58:25 +0000

    Updated signal handling

--- procs/fork_wait_child.c
+++ procs/fork_wait_child.c
@@ -13,29 +13,48 @@
 #include <signal.h>
 
 // Some Macros
-#define WaitForever while(1);
-#define WaitForEnter printf("Hit Enter to proceed :"); getchar();
+#define WaitForever while(1)
+#define WaitForEnter do { printf("Hit Enter to proceed :"); getchar();} while(0)
 
-// set HANDLER to SIG_IGN or SIG_DFL for experimentation
-#define HANDLER SIG_DFL
+// set handler to SIG_DFL for experimentation
+void (*handler)(int) =SIG_DFL ;
 
 // Note: Setting the handler for SIGCHLD to SIG_IGN tells the kernel:
 //    "when a child terminates, reap it immediately"
-void set_sig_child(){
-  if (signal(SIGCHLD, HANDLER) == SIG_ERR) {
+void set_sig_child( void (*handler)(int) ){
+
+	struct sigaction sa;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_handler = handler;
+	sa.sa_flags=0;
+
+	if (sigaction(SIGCHLD,&sa,NULL)== -1) {
        perror("setting SIGCHLD failed");
        exit(1);
-  }
-  else
-	  printf("Parent set SIGCHLD.\n");
+	}
+    printf("Parent set SIGCHLD to ** ");
+    if( handler == SIG_DFL)
+	    printf("SIG_DFL ** \n");
+    else
+	    printf("SIG_IGN ** \n");
 }
-void main(){
+
+void main(int argc, char *argv[]){
 	int child_pid;	// return value of fork
 	int status;		// used by wait
 
-	printf("Testing with signal = %d\n", (int ) HANDLER);
-
-	set_sig_child();
+	// process the argv and set eh handlerto SIG_DFL or SIG_IGN
+	if (argc>1 && strcmp(argv[1],"SIG_IGN")==0){
+		handler=SIG_IGN;
+	} else if (argc>1 && strncmp(argv[1],"-h",2)==0) {
+		printf("%s [SIG_IGN|SIG_DFL]\n",argv[0]);
+		exit(0);
+	} else
+		handler=SIG_DFL;
+	// set the handler for the SIGCHLD signal
+	set_sig_child(handler);
+    // print a confirmation message of which we are testing
+	printf("Testing with signal = %ld\n", (long ) handler);
 
 	printf("Parent PID = %d\n",(int)getpid());
 
