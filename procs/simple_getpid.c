5d6c53dad8a53ad62f7ff2a31ca92b22ddbb072a
Badrinath R <r.badrinath@gmail.com>
2026-08-20 07:49:11 +0000

    small changes

--- procs/simple_getpid.c
+++ procs/simple_getpid.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-
+#include <string.h>
 #include <unistd.h>
 
 
 
 void main(){
 	printf("My pid is: %d\n",getpid());
-	sleep(100);
+	int i;
+	// try a busy loop with different things to do
+	// correspondingly check pidstat -p <pid> 1
+	while (i=1){
+		// doing nothing is a busy while loop
+		//strcmp(" "," ");	// library call
+		//getpid();       	// system call
+		//sleep(100);	    // system call but not actively consume any CPU
+		//printf("hi\n");  // a library call leading to a system call
+	}
 }
--- procs/syscallC.c
+++ procs/syscallC.c
@@ -1,11 +1,15 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/syscall.h>
+// The above is needed for syscall numbers in symbolic form  like SYS_XXXX
+// See man 2 syscall
 
 char * message="hello from the C world\n";
+#define WR_FD 1
 
+// This is a way to make a syscall using the syscall number from C
 void main()
 {
-      syscall(SYS_write, 1, message , strlen(message));
+      syscall(SYS_write, WR_FD, message , strlen(message));
 }
 
